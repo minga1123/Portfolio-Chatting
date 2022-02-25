f1475d4a566095a78cf01937ebe0e5638ac3ca7b
Taesuuu <taesu08@gmail.com>
2022-02-25 22:23:45 +0900

    개발 준비 끝

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 //여긴 main.js 입니다.
+
+var app = new Vue({
+    el : totalDiv,
+    data : {
+        // 사용 할 변수들 
+    },
+    components : {
+        //컴포넌트 추가
+
+    },
+
+    methods : {
+        // Vue 메서드 
+
+    },
+    
+    created : function() {
+        // 소켓 연결 할 부분
+
+    }
+});
