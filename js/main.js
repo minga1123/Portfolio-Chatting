4afb0c4ed483a86472cff0ef5f115138a0954e0b
Taesuuu <taesu08@gmail.com>
2022-02-26 22:04:31 +0900

    LoginPage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 //여긴 main.js 입니다.
 
 var app = new Vue({
-    el : totalDiv,
+    el : '#totalDiv',
     data : {
         // 사용 할 변수들 
+        userNickname : null,
+        userLogin : false,
+        chatting : false,
     },
+
     components : {
         //컴포넌트 추가
 
@@ -12,6 +16,29 @@ var app = new Vue({
 
     methods : {
         // Vue 메서드 
+        getUserNickName : function() {
+            if(this.userNickname === null) {
+                return;
+            } 
+            else if(this.userNickname.length < 8){
+                //console.log("8글자 이하");
+                this.userLogin = true;
+            } 
+            else if(this.userNickname.length > 8) {
+                //console.log("9글자 이상");
+                document.querySelector(".background").className = "background show";
+                //alert("닉네임은 8글자 이하만 사용 가능합니다.");
+                this.userNickname = null;
+            }
+        },
+        goLoginPage : function() {
+            this.userLogin = false;
+            this.userNickname = null;
+        },
+
+        closepopup : function() {
+            document.querySelector(".background").className = "background";
+        }
 
     },
     
