e2725278986ce41d8ae091082c9677a4b6c751ee
minga1123 <83595244+minga1123@users.noreply.github.com>
2022-02-27 16:09:32 +0900

    Add

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 //여긴 main.js 입니다.
 
 var app = new Vue({
-    el : totalDiv,
+    el : "#TotalDiv",
     data : {
         // 사용 할 변수들 
+        userName : null,
+        Title : true
     },
     components : {
         //컴포넌트 추가
@@ -12,6 +14,15 @@ var app = new Vue({
 
     methods : {
         // Vue 메서드 
+        userJoin: function(){
+            console.log("입장하기");
+            if(this.userName!=null){
+                this.Title=false;
+            }
+        },
+        userBack: function(){
+            console.log("뒤로가기");
+        }
 
     },
     
