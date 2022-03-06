64250ea1989678d9ff9dcff7d14d376e9b3c3784
minga1123 <83595244+minga1123@users.noreply.github.com>
2022-03-06 20:20:06 +0900

    help......

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-//=====//
+//====같은 서버 접속 user 배열
 var userId = [];
 //=====정적파일 설정=====//
 const static = require('serve-static');
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 app.get('/login', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 }); 
-server.listen(3000, () => {
+server.listen(8000, () => {
     console.log("server open 3000");
 }); 
 
@@ -27,9 +27,15 @@ io.sockets.on('connection', function(socket) {
    socket.on('userlogin', function(userName){
        console.log(userName.userName + '님이 로그인했습니다.');
        userId.push(userName.userName);
-       console.log(userId + "," + userId.length + "명 접속");
+       console.log(userId);
+       socket.emit('loginInfo',{userName:userName.userName, loginCount:userId.length, userArray:userId});
+       
    });
    socket.on('userlogout', function (userName) {
        console.log(userName.userName + '님이 로그아웃했습니다.');
+       //splice(변경할 배열 인덱스 값, 제거 개수) 로그아웃한 유저를 userID배열에서 삭제
+       userId.splice(userId.indexOf(userName.userName),1);
+       console.log(userId);
+       socket.emit('loginoutInfo',{userName:userName.userName, logoutCount:userId.length, userArray:userId});
    });
 });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 
 var socket = io();
 
+//socket.emit('loginInfo',{userName:userName.userName, loginCount:userId.length, userIDArray:userId});
+var userInfo = [];
+
+
+
 var app = new Vue({
     el : "#TotalDiv",
     data : {
@@ -8,8 +13,9 @@ var app = new Vue({
         userName : null,
         Title : true,
         Chat : false,
-        userChat : null
-        
+        userChat : null,
+        //app.loginCount=serverData.loginCount;
+        loginCount : 0
     },
     components : {
         //컴포넌트 추가
@@ -22,10 +28,11 @@ var app = new Vue({
             if(this.userName!=null && this.userName.length<=8){
                 console.log("입장하기");
                 this.Title=false;
+                //입장하기 누르면 userName값 emit 로그인한 유저 확인
                 socket.emit('userlogin',{userName:this.userName});
             } else if(this.userName.length>=8){
                 console.log("8글자보다 큼");
-                //팝업
+                //팝업창 띄우기 
                 document.querySelector(".background").className = "background show";
                 this.userName=null;
                 
@@ -35,6 +42,7 @@ var app = new Vue({
         TitleBackBtn: function(){
             console.log("Main->Title 이전 버튼");
             this.Title=true;
+            //타이틀화면 이동 시 userName값 emit 로그아웃한 유저 확인
             socket.emit('userlogout',{userName:this.userName});
             this.userName=null;
             
@@ -53,6 +61,30 @@ var app = new Vue({
         },
         SendBtn: function(){
             console.log("메세지 전송 버튼");
+        },
+                //MainPage에서 같은 서버에 접속한 severData 수 만큼 동적 div 생성 (appendChild)
+        addUserDiv : function(){
+             
+            if(this.Chat) {
+                var count = document.getElementById('MainContent').childElementCount;
+                for(var i = 0; i < count; i++) {
+                    document.getElementById('MainContent').removeChild(document.getElementById('MainContent').firstChild);
+                }
+            }            
+
+            for(var i=0; i<this.loginCount; i++){
+                if(this.userName!==userInfo[i]){
+                    let div = document.createElement('div');
+                    div.className ='MainContent';
+                    let text = document.createTextNode(userInfo[i]);
+                    div.appendChild(text);
+                    //특정 부모 노드의 자식 노드 리스트 중 마지막 자식으로 붙입니다
+                    document.getElementById('MainContent').appendChild(div);
+                    // div.addEventListener('click',app.testFunction);
+                    
+                }    
+            }
+            
         }
     },
     
@@ -60,7 +92,29 @@ var app = new Vue({
         // 소켓 연결 할 부분
         socket.on('connect', function(){
             console.log('소켓 연결');
-
+            //로그인 
+            socket.on('loginInfo', function(serverData){
+                console.log(serverData);
+                app.loginCount=serverData.loginCount; 
+                // app.js => userIDArray:userId[]; === main.js => userInfo[];
+                userInfo = serverData.userArray.filter(() => true);
+                console.log(userInfo);
+                app.addUserDiv();
+                // if(app.Chat===false) {
+                //     app.addUserDiv();
+                // }
+               
+            });
+            //로그아웃 
+            socket.on('loginoutInfo', function(serverData){
+                console.log(serverData);
+                userInfo = serverData.userArray.filter(() => true);
+                app.addUserDiv();
+                // if(app.Chat===false) {
+                //     app.addUserDiv();
+                // }
+            });
+            
         });
 
     }
