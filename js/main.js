c20492c307c3411bd7a4b94043e9cd9f99d05f89
Taesuuu <taesu08@gmail.com>
2022-03-11 21:38:50 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -104,7 +104,6 @@ var app = new Vue({
             }
             socket.emit('send_Message', {sendUser : this.userNickname, requestUser : this.reqestID ,sendMessage : this.userChatting});
             this.userChatting = null;
-$
             
         },
 
