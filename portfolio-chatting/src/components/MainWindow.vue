f2d47ecdc728fe830565d68ae758466548994b3c
minga1123 <minga3211@gmail.com>
2022-04-06 23:24:25 +0900

    img

---
+++ portfolio-chatting/src/components/MainWindow.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Window">
+
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'Window'
+}
+</script>
+
+<style>
+.window{
+    width: 1440px;
+    height: 2560px;
+    
+}
+</style>
--- portfolio-chatting/src/router/index.js
+++ portfolio-chatting/src/router/index.js
@@ -16,9 +16,6 @@ const routes = [
   {
     path: '/Mainpage',
     name: 'Mainpage',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: Mainpage
   },
   {
