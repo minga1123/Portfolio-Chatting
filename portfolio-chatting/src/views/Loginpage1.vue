428092558765eaac336123e7d8e4af69d045c802
Taesuuu <taesu08@gmail.com>
2022-04-06 23:23:53 +0900

    test

---
+++ portfolio-chatting/src/views/Loginpage1.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MainWindow1>
+      
+    </MainWindow1>
+  </div>
+</template>
+
+<script>
+
+import MainWindow1 from '../components/MainWindow1.vue'
+
+export default {
+  components: {
+    MainWindow1 : MainWindow1,
+  },
+};
+</script>
+
+<style></style>
