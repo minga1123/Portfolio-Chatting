d210dbd0f4a23db136172311b356b83fd884d095
Taesuuu <taesu08@gmail.com>
2022-04-06 23:40:05 +0900

    Add LoginPage

--- portfolio-chatting/src/views/Loginpage1.vue
+++ portfolio-chatting/src/views/Loginpage1.vue
@@ -1,20 +1,30 @@
 <template>
-  <div>
-    <MainWindow1>
-      
-    </MainWindow1>
+  <div id="MainWindow1">
+      <div id="imgDiv">
+        <img id="loginIMG" src="../assets/pin.png"> 
+      </div>
+      <LoginComponent1></LoginComponent1>
   </div>
 </template>
 
 <script>
 
-import MainWindow1 from '../components/MainWindow1.vue'
-
+import LoginComponent1 from '../components/LoginComponent1.vue'
 export default {
+
   components: {
-    MainWindow1 : MainWindow1,
+    LoginComponent1 : LoginComponent1,
   },
 };
 </script>
 
-<style></style>
+<style>
+#MainWindow1 {
+  width: 2560px;
+  height: 1440px;
+}
+#imgDiv{
+  width: 100%;
+  height: 100%;
+}
+</style>
